6e1847ac5142c200cfa25ad20b3307dfb0336b6d
Egor K <egor.k@smartum.pro>
2019-09-29 16:06:40 +0300

    💣👊🏻🐼

---
+++ build/precache-manifest.f8f5409e7e08ff92953cb48584e0f9ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60c5e5fd207c576c8c7fb8f92ff04ef7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ab9f934edf8f91ac686",
+    "url": "/static/css/main.9cb52921.chunk.css"
+  },
+  {
+    "revision": "3b85ffaa458644a5ad3e",
+    "url": "/static/js/2.4e9c98c0.chunk.js"
+  },
+  {
+    "revision": "0ab9f934edf8f91ac686",
+    "url": "/static/js/main.c0ab14d7.chunk.js"
+  },
+  {
+    "revision": "cea6dd0cedfd46cca495",
+    "url": "/static/js/runtime-main.178b941e.js"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/static/media/logo.0c089234.svg"
+  }
+]);
